b04a39a29e5d978417bf694f3372b4e3565bd132
kswizzlek <kid7kruger@gmail.com>
2021-02-24 22:08:31 -0500

    updated validaiton logic

--- validation/validateTripDates.ts
+++ validation/validateTripDates.ts
@@ -4,7 +4,7 @@ export const validateTripDates = {
     checkIfDateOverlaps: (tripDate: TripDate, tripDates: TripDates): boolean => {
         if(!tripDates.dates) return false;
         let invalid = false;
-        tripDates.dates.forEach(td => {
+        tripDates.dates.filter(d => d.personsUsername === tripDate.personsUsername).forEach(td => {
             if ((new Date(tripDate.startDate) > new Date(td.startDate)) && (new Date(tripDate.startDate) < new Date(td.endDate))){
                 invalid = true;
             }
